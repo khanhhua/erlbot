27ad36c8d6dea8ffd9214aad64d24d016ee0305f
Khanh Hua <khanh.hua@axa.com.sg>
2017-08-22 14:34:46 +0800

    Standard FSM code

---
+++ src/erlbot_bot.erl
@@ -0,0 +1,263 @@
+%% @author khanhhua
+%% @doc @todo Add description to erlbot_bot.
+
+
+-module(erlbot_bot).
+-behaviour(gen_fsm).
+-export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+-export([greeting/2, greeting/3, listening/2, listening/3, guiding/2, guiding/3]).
+
+-define(SERVER, ?MODULE).
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([start_link/1, stop/0, tell/1]).
+
+
+
+%% ====================================================================
+%% Behavioural functions
+%% ====================================================================
+-record(conversation, {id, username, topic, messages=[]}).
+-record(message, {who, text}).
+
+%% start_link/1
+%% ========== 
+start_link(Username) ->
+    gen_fsm:start_link({local, ?SERVER}, ?MODULE, #conversation{id=1, username=Username}, []).
+
+stop() ->
+    gen_fsm:stop(?SERVER).
+
+tell(TextMessage) ->
+    gen_fsm:send_event(?SERVER, #message{who=user, text=TextMessage}).
+
+%% init/1
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:init-1">gen_fsm:init/1</a>
+-spec init(Args :: term()) -> Result when
+	Result :: {ok, StateName, StateData}
+			| {ok, StateName, StateData, Timeout}
+			| {ok, StateName, StateData, hibernate}
+			| {stop, Reason}
+			| ignore,
+	StateName :: atom(),
+	StateData :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: term().
+%% ====================================================================
+init(Conversation) ->
+    {ok, greeting, Conversation}.
+
+
+%% greeting/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:StateName-2">gen_fsm:StateName/2</a>
+-spec greeting(Event :: timeout | term(), StateData :: term()) -> Result when
+	Result :: {next_state, NextStateName, NewStateData}
+			| {next_state, NextStateName, NewStateData, Timeout}
+			| {next_state, NextStateName, NewStateData, hibernate}
+			| {stop, Reason, NewStateData},
+	NextStateName :: atom(),
+	NewStateData :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: term().
+%% ====================================================================
+% @todo implement actual conversation
+greeting(Event, StateData) ->
+    io:format("Michelle is greeting...~n"),
+    {next_state, listening, StateData, 5000}.
+
+%% greeting/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:StateName-3">gen_fsm:StateName/3</a>
+-spec greeting(Event :: term(), From :: {pid(), Tag :: term()}, StateData :: term()) -> Result when
+	Result :: {reply, Reply, NextStateName, NewStateData}
+			| {reply, Reply, NextStateName, NewStateData, Timeout}
+			| {reply, Reply, NextStateName, NewStateData, hibernate}
+			| {next_state, NextStateName, NewStateData}
+			| {next_state, NextStateName, NewStateData, Timeout}
+			| {next_state, NextStateName, NewStateData, hibernate}
+			| {stop, Reason, Reply, NewStateData}
+			| {stop, Reason, NewStateData},
+	Reply :: term(),
+	NextStateName :: atom(),
+	NewStateData :: atom(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: normal | term().
+%% ====================================================================
+greeting(Event, From, StateData) ->
+    io:format("Michelle is greeting...~n"),
+    Reply = ok,
+    {reply, Reply, listening, StateData}.
+
+%% listening/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:StateName-3">gen_fsm:StateName/3</a>
+-spec listening(Event :: term(), From :: {pid(), Tag :: term()}, StateData :: term()) -> Result when
+    Result :: {reply, Reply, NextStateName, NewStateData}
+            | {reply, Reply, NextStateName, NewStateData, Timeout}
+            | {reply, Reply, NextStateName, NewStateData, hibernate}
+            | {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, Reply, NewStateData}
+            | {stop, Reason, NewStateData},
+    Reply :: term(),
+    NextStateName :: atom(),
+    NewStateData :: atom(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: normal | term().
+%% ====================================================================
+listening(Event, From, StateData) ->
+    Reply = ok,
+    {reply, Reply, listening, StateData}.
+
+%% listening/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:StateName-2">gen_fsm:StateName/2</a>
+-spec listening(Event :: timeout | term(), StateData :: term()) -> Result when
+    Result :: {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, NewStateData},
+    NextStateName :: atom(),
+    NewStateData :: term(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: term().
+%% ====================================================================
+%% @todo implement actual conversation
+listening(Event, StateData) ->
+    io:format("Michelle is listening to your request...~n"),
+    % If conversation has one reference to "bus", then begin to guide
+    case Event of
+        #message{who=user, text="Bus how long"} -> {next_state, guiding, StateData#conversation{topic=bus}};
+        timeout -> io:format("Michelle: Sorry but are you still there...~n")
+    end.
+    
+
+%% guiding/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:StateName-3">gen_fsm:StateName/2</a>
+-spec guiding(Event :: term(), StateData :: term()) -> Result when
+    Result :: {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, NewStateData},
+    NextStateName :: atom(),
+    NewStateData :: term(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: term().
+%% ====================================================================
+guiding(Event, StateData) ->
+    io:format("Michelle is guiding user...~n"),
+    {next_state, listening, StateData}.
+
+%% guiding/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:StateName-3">gen_fsm:StateName/3</a>
+-spec guiding(Event :: term(), From :: {pid(), Tag :: term()}, StateData :: term()) -> Result when
+    Result :: {reply, Reply, NextStateName, NewStateData}
+            | {reply, Reply, NextStateName, NewStateData, Timeout}
+            | {reply, Reply, NextStateName, NewStateData, hibernate}
+            | {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, Reply, NewStateData}
+            | {stop, Reason, NewStateData},
+    Reply :: term(),
+    NextStateName :: atom(),
+    NewStateData :: atom(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: normal | term().
+%% ====================================================================
+guiding(Event, From, StateData) ->
+    Reply = ok,
+    {reply, Reply, listening, StateData}.
+
+%% handle_event/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:handle_event-3">gen_fsm:handle_event/3</a>
+-spec handle_event(Event :: term(), StateName :: atom(), StateData :: term()) -> Result when
+	Result :: {next_state, NextStateName, NewStateData}
+			| {next_state, NextStateName, NewStateData, Timeout}
+			| {next_state, NextStateName, NewStateData, hibernate}
+			| {stop, Reason, NewStateData},
+	NextStateName :: atom(),
+	NewStateData :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: term().
+%% ====================================================================
+handle_event(Event, StateName, StateData) ->
+    {next_state, StateName, StateData}.
+
+
+%% handle_sync_event/4
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:handle_sync_event-4">gen_fsm:handle_sync_event/4</a>
+-spec handle_sync_event(Event :: term(), From :: {pid(), Tag :: term()}, StateName :: atom(), StateData :: term()) -> Result when
+	Result :: {reply, Reply, NextStateName, NewStateData}
+			| {reply, Reply, NextStateName, NewStateData, Timeout}
+			| {reply, Reply, NextStateName, NewStateData, hibernate}
+			| {next_state, NextStateName, NewStateData}
+			| {next_state, NextStateName, NewStateData, Timeout}
+			| {next_state, NextStateName, NewStateData, hibernate}
+			| {stop, Reason, Reply, NewStateData}
+			| {stop, Reason, NewStateData},
+	Reply :: term(),
+	NextStateName :: atom(),
+	NewStateData :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: term().
+%% ====================================================================
+handle_sync_event(Event, From, StateName, StateData) ->
+    Reply = ok,
+    {reply, Reply, StateName, StateData}.
+
+
+%% handle_info/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:handle_info-3">gen_fsm:handle_info/3</a>
+-spec handle_info(Info :: term(), StateName :: atom(), StateData :: term()) -> Result when
+	Result :: {next_state, NextStateName, NewStateData}
+			| {next_state, NextStateName, NewStateData, Timeout}
+			| {next_state, NextStateName, NewStateData, hibernate}
+			| {stop, Reason, NewStateData},
+	NextStateName :: atom(),
+	NewStateData :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: normal | term().
+%% ====================================================================
+handle_info(Info, StateName, StateData) ->
+    {next_state, StateName, StateData}.
+
+
+%% terminate/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:terminate-3">gen_fsm:terminate/3</a>
+-spec terminate(Reason, StateName :: atom(), StateData :: term()) -> Result :: term() when
+	Reason :: normal
+			| shutdown
+			| {shutdown, term()}
+			| term().
+%% ====================================================================
+terminate(Reason, StateName, StatData) ->
+    ok.
+
+
+%% code_change/4
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:code_change-4">gen_fsm:code_change/4</a>
+-spec code_change(OldVsn, StateName :: atom(), StateData :: term(), Extra :: term()) -> {ok, NextStateName :: atom(), NewStateData :: term()} when
+	OldVsn :: Vsn | {down, Vsn},
+	Vsn :: term().
+%% ====================================================================
+code_change(OldVsn, StateName, StateData, Extra) ->
+    {ok, StateName, StateData}.
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
