b11baf0295f1c8f0e39b09d9ea3e86e2c455ff04
Khanh Hua <giakhanh2487@gmail.com>
2017-09-04 07:51:35 +0800

    Search over context for action params

--- src/erlbot_ai.erl
+++ src/erlbot_ai.erl
@@ -20,6 +20,8 @@
 %% -H 'Authorization:Bearer ab8bc71108284909a45b9e86d4534e31'
 
 query(Text, [{sessionId, SessionId}]) ->
+  query(Text, [{sessionId, SessionId}, {context, undefined}]);
+query(Text, [{sessionId, SessionId}, {context, Context}]) ->
   QueryParams = [
     {"v", "20150910"},
     {"lang", "en"},
@@ -43,28 +45,52 @@ query(Text, [{sessionId, SessionId}]) ->
     httpc:request(get, {Url, Headers}, [], []),
 
   Data = jsx:decode(list_to_binary(Body)),
-  Intent = generate_intent(Data),
+  Intent = generate_intent(Data, Context),
 
   io:format("Intent: ~p ~n", [Intent]),
   {ok, Intent}.
 
-generate_intent(Data) ->
+generate_intent(Data, Context) ->
   Result = proplists:get_value(<<"result">>, Data),
   Action = proplists:get_value(<<"action">>, Result),
   Parameters = proplists:get_value(<<"parameters">>, Result, []),
+  Contexts = proplists:get_value(<<"contexts">>, Result, []),
 
   io:format("generate_intent~n- Action: ~p ~n- Parameters: ~p~n", [Action, Parameters]),
+  io:format("Context: ~p~n", [Contexts]),
 
-  #intent{
+  Intent = #intent{
     action =
-      case Action of
-        undefined -> unknown;
-        _ ->  binary_to_list(Action)
-      end,
+    case Action of
+      undefined -> unknown;
+      _ ->  binary_to_list(Action)
+    end,
     parameters = lists:foldl(
       fun
         ({_Key, <<>>}, Acc0) -> Acc0;
         ({Key, Value}, Acc0) -> [{binary_to_list(Key), binary_to_list(Value)},Acc0]
       end,
       [], Parameters)
-  }.
+  },
+
+  ContextDefinedParameters = case lists:filter(
+    fun (Item) ->
+      Name = proplists:get_value(<<"name">>, Item),
+      binary_to_list(Name) =:= Context
+    end, Contexts) of
+    [ContextDefined0] -> proplists:get_value(<<"parameters">>, ContextDefined0);
+    [] -> undefined
+  end,
+
+  if
+    (length(Contexts) =:= 0) or (ContextDefinedParameters =:= undefined) -> Intent;
+    true ->
+      ContextualParameters = lists:foldl(
+        fun ({Key, Value}, Acc0) ->
+          case proplists:get_value(Key, ContextDefinedParameters) of
+            undefined -> [{Key, Value} | Acc0];
+            ContextDefinedValue -> [{Key, ContextDefinedValue} | Acc0]
+          end
+        end, [], Intent#intent.parameters),
+      Intent#intent{parameters = ContextualParameters}
+  end.
